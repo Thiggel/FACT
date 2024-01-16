02c047d734a21eae347fa791512a720ed4f3844f
Max Belitsky <md.belitsky@gmail.com>
2024-01-16 12:44:30 +0100

    Add a set_seed function

--- fairgraph/utils/utils.py
+++ fairgraph/utils/utils.py
@@ -50,3 +50,21 @@ def set_device():
     except:
         device = torch.device('cpu')
     return device
+
+def set_seed(seed):
+    """
+    Function for setting the seed for reproducibility.
+    """
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+    try:
+        if torch.backends.mps.is_available():
+            torch.mps.manual_seed(seed)
+    except:
+        pass
