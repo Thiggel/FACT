4d57024b16fb2f100651258dd5b7bcf840ca2c99
Max Belitsky <md.belitsky@gmail.com>
2024-01-16 16:49:27 +0100

    Replace hardocded hyperparameters with arguments in Experiment

--- fairgraph/experiment.py
+++ fairgraph/experiment.py
@@ -5,6 +5,7 @@ from .dataset import POKEC, NBA
 
 import time
 
+# TODO: go through all the models and replace hardcoded hyperparemters with arguments, then add to hyperparams file
 
 class Experiment:
     r"""
@@ -12,13 +13,37 @@ class Experiment:
     """
 
     def __init__(self, dataset_name, device=None, epochs=10_000, test_epochs=1_000,
-                 lr=1e-4, weight_decay=1e-5):
+                 lr=1e-4, weight_decay=1e-5, g_temperature=1.0, g_hidden=64,
+                 f_hidden=64, f_layers=2, f_dropout=0.1, f_output_features=64,
+                 k_hidden=64, k_output_features=64, c_hidden=64, c_input=64,
+                 warmup=0):
         self.device = device if device else set_device()
         self.epochs = epochs
         self.test_epochs = test_epochs
         self.lr = lr
         self.weight_decay = weight_decay
         self.dataset = self.initialize_dataset(dataset_name)
+
+        # Augmentation model g hyperparameters
+        self.g_temperature = g_temperature
+        self.g_hidden = g_hidden
+
+        # Encoder model f hyperparameters
+        self.f_hidden = f_hidden
+        self.f_dropout = f_dropout
+        self.f_layers = f_layers
+        self.f_output_features = f_output_features
+
+        # Adversary model k hyperparameters
+        self.k_hidden = k_hidden
+        self.k_output_features = k_output_features
+
+        # Classifier model hyperparameters
+        self.c_hidden = c_hidden
+        self.c_input = c_input
+
+        # Graphair model hyperparameters
+        self.warmup = warmup
 
     def initialize_dataset(self, dataset_name):
         if dataset_name == Datasets.NBA:
@@ -68,26 +93,64 @@ class Experiment:
         adj = self.dataset.adj
         idx_sens = self.dataset.idx_sens_train
 
-        # Initialize a model
-        self.aug_model = aug_module(features, n_hidden=64, temperature=1, device=self.device).to(self.device)
-        self.f_encoder = GCN_Body(in_feats=features.shape[1], n_hidden=64, out_feats=64, dropout=0.1, nlayer=2).to(self.device)
-        self.sens_model = GCN(in_feats = features.shape[1], n_hidden=64, out_feats=64, nclass=1).to(self.device)
-        self.classifier_model = Classifier(input_dim=64, hidden_dim=64)
-        self.model = Graphair(aug_model=self.aug_model, f_encoder=self.f_encoder, sens_model=self.sens_model,
-                         classifier_model=self.classifier_model, lr=self.lr, weight_decay=self.weight_decay,
-                         dataset=dataset_name).to(self.device)
+        # Initialize augmentation model g
+        self.aug_model = aug_module(
+            features=features,
+            n_hidden=self.g_hidden,
+            temperature=self.g_temperature,
+            device=self.device
+            ).to(self.device)
+
+        # Initialize encoder model f
+        self.f_encoder = GCN_Body(
+            in_feats=features.shape[1],
+            n_hidden=self.f_hidden,
+            out_feats=self.f_output_features,
+            dropout=self.f_dropout,
+            nlayer=self.f_layers
+            ).to(self.device)
+
+        # Initialize adversary model k
+        self.sens_model = GCN(
+            in_feats=features.shape[1],
+            n_hidden=self.k_hidden,
+            out_feats=self.k_output_features,
+            nclass=1
+            ).to(self.device)
+
+        # Initialize classifier for testing
+        self.classifier_model = Classifier(input_dim=self.c_input, hidden_dim=self.c_hidden)
+
+        # Initialize the Graphair model
+        self.model = Graphair(
+            aug_model=self.aug_model,
+            f_encoder=self.f_encoder,
+            sens_model=self.sens_model,
+            classifier_model=self.classifier_model,
+            lr=self.lr, # TODO: add a separate lr for classifier model
+            weight_decay=self.weight_decay, # TODO: add a separate weight_decay for classifier model
+            dataset=dataset_name
+            ).to(self.device)
 
         # Train the model
         st_time = time.time()
-        self.model.fit_whole(epochs=self.epochs, adj=adj, x=features, sens=sens, idx_sens=idx_sens, warmup=0, adv_epoches=1)
+        self.model.fit_whole(
+            epochs=self.epochs,
+            adj=adj,
+            x=features,
+            sens=sens,
+            idx_sens=idx_sens,
+            warmup=self.warmup,
+            adv_epoches=1) # TODO: figure out what adv_epochs is
         print("Training time: ", time.time() - st_time)
 
         # Test the model
-        self.model.test(adj=adj,
-                   features=features,
-                   labels=self.dataset.labels,
-                   epochs=self.test_epochs,
-                   idx_train=self.dataset.idx_train,
-                   idx_val=self.dataset.idx_val,
-                   idx_test=self.dataset.idx_test,
-                   sens=sens)
+        self.model.test(
+            adj=adj,
+            features=features,
+            labels=self.dataset.labels,
+            epochs=self.test_epochs,
+            idx_train=self.dataset.idx_train,
+            idx_val=self.dataset.idx_val,
+            idx_test=self.dataset.idx_test,
+            sens=sens)
