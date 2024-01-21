cdc1de447d99659c8d08403a843534c27dbc0f4b
flaitenberger <f.laitenberger@gmail.com>
2024-01-21 12:19:04 +0100

    created class for synthetic dataset creation

---
+++ fairgraph/dataset/extension_dataset.py
@@ -0,0 +1,95 @@
+import pickle
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import random
+
+
+class ExtendDatasetWithIncomeAndEducation:
+    def __init__(self, path, seed = 42):
+        self.set_seed(seed)
+        self.path = path
+        self.graph = self._open()
+        self.arr_for_visualization = []
+
+    def set_seed(seed):
+        random.seed(seed)
+        np.random.seed(seed)
+    
+    def _open(self):
+        with open(self.path, 'rb') as f:
+            graph = pickle.load(f)
+
+        return graph
+
+    def _sample_and_add_income(self, attributes):
+        p_minority_income = [0.9, 0.1]
+        p_majority_income = [0.1, 0.9]
+
+        p_current = p_majority_income \
+            if attributes['m'] == 1 \
+            else p_minority_income
+
+        attributes['income'] = np.random.choice([0, 1], p=p_current)
+
+    def _sample_and_add_education(self, attributes):
+        p_minority_education = [0.7, 0.3]
+        p_majority_education = [0.3, 0.7]
+
+        p_current = p_majority_education \
+            if attributes['m'] == 1 \
+            else p_minority_education
+
+        attributes['education'] = np.random.choice([0, 1], p=p_current)
+
+    def _add_attributes(self):
+        for u in self.graph.nodes:
+            attributes = self.graph.nodes[u]
+            self._sample_and_add_income(attributes)
+            self._sample_and_add_education(attributes)
+
+            self.arr_for_visualization.append(self.graph.nodes[u])
+
+    def _save(self):
+        with open(self.path, 'wb') as f:
+            pickle.dump(self.graph, f)
+
+    def process(self):
+        self._add_attributes()
+        self._save()
+        print('Saved!')
+
+    def visualize(self, filename):
+        array = np.array([list(d.values()) for d in self.arr_for_visualization])
+
+        df = pd.DataFrame(array, columns=['m', 'income', 'education'])
+
+        counts = df.groupby(['m', 'income', 'education']).size() \
+            .reset_index(name='count')
+
+        fig, axes = plt.subplots(2, 2, figsize=(10, 10))
+        for i, group in enumerate(counts['m'].unique()):
+            for j, variable in enumerate(['income', 'education']):
+                sizes = [
+                    counts[counts['m'] == group][
+                        counts[variable] == 0
+                    ]['count'].sum(),
+                    counts[counts['m'] == group][
+                        counts[variable] == 1
+                    ]['count'].sum()
+                ]
+
+                labels = ['0', '1']
+                axes[i, j].pie(sizes, labels=labels, autopct='%1.1f%%')
+                axes[i, j].set_title(f'm = {group}, {variable}')
+
+        plt.savefig(filename)
+
+
+
+
+
+
+dataset = ExtendDatasetWithIncomeAndEducation('dataset/artificial/DPAH-N1000-fm0.3-d0.03-ploM2.5-plom2.5-hMM0.2-hmm0.2-ID0.gpickle')
+dataset.process()
+dataset.visualize('pie-chart.png')
--- fairgraph/dataset/fairgraph_dataset.py
+++ fairgraph/dataset/fairgraph_dataset.py
@@ -7,6 +7,119 @@ import random
 from torch_geometric.data import download_url
 
 
+class ArtificialSensitiveGraphDataset():
+    def __init__(self, root, device='cpu'):
+        self.name = "artificial_sensitive_graph"
+        self.root = root
+        self.sens_attr = "region"
+        self.predict_attr = "I_am_working_in_field"
+        self.label_number = 50000
+        self.sens_number = 20000
+        self.seed = 20
+        self.test_idx=False
+        self.data_path = data_path
+        self.device=device
+        self.process()
+    
+    @property
+    def raw_paths(self):
+        return [f"{self.dataset}.csv",f"{self.dataset}_relationship.txt",f"{self.dataset}.embedding"]
+    
+    def download(self):
+        print('downloading raw files from:', self.data_path)
+        if not os.path.exists(self.root):
+            os.makedirs(self.root)
+        
+        for raw_path in self.raw_paths:
+            download_url(self.data_path+raw_path,self.root)
+
+    def read_graph(self):
+        self.download()
+        print(f'Loading {self.dataset} dataset from {os.path.abspath(self.root+"/"+self.raw_paths[0])}')
+        # raw_paths[0] will be region_job.csv
+        idx_features_labels = pd.read_csv(os.path.abspath(self.root+"/"+self.raw_paths[0]))
+        header = list(idx_features_labels.columns)
+        header.remove("user_id")
+
+        header.remove(self.sens_attr)
+        header.remove(self.predict_attr)
+
+
+        features = sp.csr_matrix(idx_features_labels[header], dtype=np.float32)
+        labels = idx_features_labels[self.predict_attr].values
+        
+
+        # build graph
+        idx = np.array(idx_features_labels["user_id"], dtype=int)
+        idx_map = {j: i for i, j in enumerate(idx)}
+        # raw_paths[1] will be region_relationship.txt
+        edges_unordered = np.genfromtxt(os.path.abspath(self.root+"/"+self.raw_paths[1]), dtype=int)
+
+        edges = np.array(list(map(idx_map.get, edges_unordered.flatten())),
+                        dtype=int).reshape(edges_unordered.shape)
+        adj = sp.coo_matrix((np.ones(edges.shape[0]), (edges[:, 0], edges[:, 1])),
+                            shape=(labels.shape[0], labels.shape[0]),
+                            dtype=np.float32)
+        # build symmetric adjacency matrix
+        adj = adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
+
+        # features = normalize(features)
+        adj = adj + sp.eye(adj.shape[0])
+
+        features = torch.FloatTensor(np.array(features.todense()))
+        labels = torch.LongTensor(labels)
+        # adj = sparse_mx_to_torch_sparse_tensor(adj)
+
+        
+        random.seed(self.seed)
+        label_idx = np.where(labels>=0)[0]
+        random.shuffle(label_idx)
+        idx_train = label_idx[:min(int(0.1 * len(label_idx)),self.label_number)]
+        idx_val = label_idx[int(0.1 * len(label_idx)):int(0.2 * len(label_idx))]
+        if self.test_idx:
+            idx_test = label_idx[self.label_number:]
+            idx_val = idx_test
+        else:
+            idx_test = label_idx[int(0.2 * len(label_idx)):]
+
+        sens = idx_features_labels[self.sens_attr].values
+
+        sens_idx = set(np.where(sens >= 0)[0])
+        idx_test = np.asarray(list(sens_idx & set(idx_test)))
+        sens = torch.FloatTensor(sens)
+        idx_sens_train = torch.LongTensor(list(sens_idx))
+
+        idx_train = torch.LongTensor(idx_train)
+        idx_val = torch.LongTensor(idx_val)
+        idx_test = torch.LongTensor(idx_test)
+
+        return adj, features, labels, idx_train, idx_val, idx_test, sens,idx_sens_train
+
+
+    def feature_norm(self,features):
+        min_values = features.min(axis=0)[0]
+        max_values = features.max(axis=0)[0]
+
+        return 2*(features - min_values).div(max_values-min_values) - 1
+
+    def process(self):
+        adj, features, labels, idx_train, idx_val, idx_test,sens,idx_sens_train = self.read_graph()
+        features = self.feature_norm(features)
+
+        labels[labels>1]=1
+        sens[sens>0]=1
+
+        self.features = features.to(self.device)
+        self.labels = labels.to(self.device)
+        self.idx_train = idx_train.to(self.device)
+        self.idx_val = idx_val.to(self.device)
+        self.idx_test = idx_test.to(self.device)
+        self.sens = sens.to(self.device)
+        self.idx_sens_train = idx_sens_train.long().to(self.device)
+
+        self.adj = adj
+
+
 class POKEC():
     r"""Pokec is a social network dataset. Two `different datasets <https://github.com/EnyanDai/FairGNN/tree/main/dataset/pokec>`_ (namely pokec_z and pokec_n) are sampled
         from the original `Pokec dataset <https://snap.stanford.edu/data/soc-pokec.html>`_.
