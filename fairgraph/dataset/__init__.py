47021fd3e1017a67d0a081bbf96d935cd13205e0
flaitenberger <f.laitenberger@gmail.com>
2024-01-23 18:23:02 +0100

    bug fix: accidentally deleted important stuff in dataset/__init__.py

--- fairgraph/dataset/__init__.py
+++ fairgraph/dataset/__init__.py
@@ -4,3 +4,7 @@ import sys
 GRAPHSAINT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 
 sys.path.append(GRAPHSAINT_ROOT)
+
+from .fairgraph_dataset import POKEC, NBA, ArtificialSensitiveGraphDataset
+
+__all__ = [POKEC, NBA, ArtificialSensitiveGraphDataset]
--- test_homopholy.sh
+++ test_homopholy.sh
@@ -1,6 +1,6 @@
-python3 run.py --device gpu --model SYNTHETIC --hMM 0.2 --hmm 0.2
-python3 run.py --device gpu --model SYNTHETIC --hMM 0.2 --hmm 0.8
-python3 run.py --device gpu --model SYNTHETIC --hMM 0.5 --hmm 0.5
-python3 run.py --device gpu --model SYNTHETIC --hMM 0.8 --hmm 0.2
-python3 run.py --device gpu --model SYNTHETIC --hMM 0.8 --hmm 0.8
+python3 run.py --device gpu --model SYNTHETIC --hMM 0.2 --hmm 0.2 --grid_search
+python3 run.py --device gpu --model SYNTHETIC --hMM 0.2 --hmm 0.8 --grid_search
+python3 run.py --device gpu --model SYNTHETIC --hMM 0.5 --hmm 0.5 --grid_search
+python3 run.py --device gpu --model SYNTHETIC --hMM 0.8 --hmm 0.2 --grid_search
+python3 run.py --device gpu --model SYNTHETIC --hMM 0.8 --hmm 0.8 --grid_search
 
