c05f974fd5535b1f41a595b2bc7451a233063844
root <denys.sheremet@gmail.com>
2024-01-30 15:57:02 +0000

    added checkpoints for the best hyperparameters for each of the datasets

--- scripts/train_models_best_params.sh
+++ scripts/train_models_best_params.sh
@@ -1,23 +1,24 @@
 hyperparams_dir="experiments/experiment_2"
 
+# train a model with the best hyperparameters once for each dataset
+
 python run.py \
     --params_file $hyperparams_dir/nba_best_hyperparameters.yml \
     --device cuda \
     --dataset NBA \
-    --experiment_name experiment_8_GAT_nba \
-    --attention
+    --experiment_name best_model_training_nba \
+    --n_runs 1
     
 python run.py \
     --params_file $hyperparams_dir/pokec_n_best_hyperparameters.yml \
     --device cuda \
     --dataset_name POKEC_N \
-    --experiment_name experiment_8_GAT_pokec_n \
-    --attention
+    --experiment_name best_model_training_pokec_n \
+    --n_runs 1
 
 python run.py \
     --params_file $hyperparams_dir/pokec_z_best_hyperparameters.yml \
     --device cuda \
     --dataset POKEC_Z \
-    --experiment_name experiment_8_GAT_pokec_z \
-    --attention
+    --experiment_name best_model_training_pokec_z \     --n_runs 1 
 
