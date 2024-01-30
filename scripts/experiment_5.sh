e9a2d6213409a50329299423dd4b59ba8a006831
Max Belitsky <md.belitsky@gmail.com>
2024-01-30 14:24:05 +0100

    Update the experiment running script

--- scripts/experiment_5.sh
+++ scripts/experiment_5.sh
@@ -1,9 +1,4 @@
 # Train a supervised model on the original graph with no augmentations from Graphair
-python3 run.py --experiment_name "experiment_5_1_NBA" --device cuda --dataset NBA --n_tests 5 --skip_graphair --params_file experiments/experiment_2/nba_best_hyperparameters.yml
-python3 run.py --experiment_name "experiment_5_1_POKEC_N" --device cuda --dataset POKEC_N --n_tests 5 --skip_graphair --params_file experiments/experiment_2/pokec_n_best_hyperparameters.yml
-python3 run.py --experiment_name "experiment_5_1_POKEC_Z" --device cuda --dataset POKEC_Z --n_tests 5 --skip_graphair --params_file experiments/experiment_2/pokec_z_best_hyperparameters.yml
-
-# Train a GCN classifier on the output of Graphair to be able to compare with the above
-python3 run.py --experiment_name "experiment_5_2_NBA" --device cuda --dataset NBA --n_runs 1 --n_tests 5 --use_gcn_classifier --params_file experiments/experiment_2/nba_best_hyperparameters.yml
-python3 run.py --experiment_name "experiment_5_2_POKEC_N" --device cuda --dataset POKEC_N --n_runs 1 --n_tests 5 --use_gcn_classifier --params_file experiments/experiment_2/pokec_n_best_hyperparameters.yml
-python3 run.py --experiment_name "experiment_5_2_POKEC_Z" --device cuda --dataset POKEC_Z --n_runs 1 --n_tests 5 --use_gcn_classifier --params_file experiments/experiment_2/pokec_z_best_hyperparameters.yml
+python3 run.py --experiment_name "experiment_5_NBA" --device cuda --dataset NBA --n_tests 5 --skip_graphair --params_file experiments/experiment_2/nba_best_hyperparameters.yml
+python3 run.py --experiment_name "experiment_5_POKEC_N" --device cuda --dataset POKEC_N --n_tests 5 --skip_graphair --params_file experiments/experiment_2/pokec_n_best_hyperparameters.yml
+python3 run.py --experiment_name "experiment_5_POKEC_Z" --device cuda --dataset POKEC_Z --n_tests 5 --skip_graphair --params_file experiments/experiment_2/pokec_z_best_hyperparameters.yml
