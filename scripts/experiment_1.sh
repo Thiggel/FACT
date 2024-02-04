db7f2b4164b54a661c7d11264d8f7940150851a5
Nordin Belkacemi <belkaceminordin@gmail.com>
2024-02-04 13:39:08 +0100

    experiment 1

--- scripts/experiment_1.sh
+++ scripts/experiment_1.sh
@@ -2,14 +2,20 @@
 python run.py \
     --dataset_name NBA \
     --grid_search \
+    --n_runs 1 \
+    --n_test 5 \
     --experiment_name NBA_grid_search_original
 
 python run.py \
     --dataset_name POKEC_N \
     --grid_search \
+    --n_runs 1 \
+    --n_tests 5 \
     --experiment_name POKEC_N_grid_search_original
 
 python run.py \
     --dataset_name POKEC_Z \
     --grid_search \
+    --n_runs 1 \
+    --n_tests 5 \
     --experiment_name POKEC_Z_grid_search_original
