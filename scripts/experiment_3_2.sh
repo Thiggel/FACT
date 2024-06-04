ef2866df0caaf56088169dcccc8c11c692cf4178
Belitsky Max <scur0209@int6.local.snellius.surf.nl>
2024-06-04 14:42:51 +0200

    Add job files

---
+++ scripts/experiment_3_2.sh
@@ -0,0 +1,15 @@
+hyperparams_dir="experiments/experiment_3_2"
+
+# train a model with the best hyperparameters once for each dataset
+    
+python run.py \
+    --params_file $hyperparams_dir/pokec_n_best_hyperparameters.yml \
+    --dataset_name POKEC_N \
+    --n_runs 5 \
+    --experiment_name POKEC_N_best_hparams_larger_batch
+
+python run.py \
+    --params_file $hyperparams_dir/pokec_z_best_hyperparameters.yml \
+    --dataset POKEC_Z \
+    --n_runs 5 \
+    --experiment_name POKEC_Z_best_hparams_larger_batch
