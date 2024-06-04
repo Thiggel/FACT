8e09ccae1c79ec84c3a7c3e11f2bfefbb0d46b1e
Filipe Laitenberger <scur1007@int4.local.snellius.surf.nl>
2024-06-04 15:52:09 +0200

    job file improvmenets

--- scripts/experiment_8.sh
+++ scripts/experiment_8.sh
@@ -4,16 +4,19 @@ python run.py \
     --params_file $hyperparams_dir/nba_best_hyperparameters.yml \
     --dataset NBA \
     --attention \
-    --experiment_name NBA_GAT
+    --experiment_name NBA_GAT_GS \
+    --grid_search 
     
 python run.py \
     --params_file $hyperparams_dir/pokec_n_best_hyperparameters.yml \
     --dataset_name POKEC_N \
     --attention \
-    --experiment_name POKEC_N_GAT
+    --experiment_name POKEC_N_GAT_GS \
+    --grid_search 
 
 python run.py \
     --params_file $hyperparams_dir/pokec_z_best_hyperparameters.yml \
     --dataset POKEC_Z \
     --attention \
-    --experiment_name POKEC_Z_GAT
+    --experiment_name POKEC_Z_GAT_GS \
+    --grid_search
