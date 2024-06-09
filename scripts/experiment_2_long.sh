4014affc0e9535294c072ec91d2c9edfa85beb3b
Belitsky Max <scur0209@int4.local.snellius.surf.nl>
2024-06-09 09:31:21 +0200

    Add scripts and jobs for longer training

--- scripts/experiment_2_long.sh
+++ scripts/experiment_2_long.sh
@@ -1,11 +1,15 @@
+hyperparams_dir="experiments/experiment_2/experiment_2_2"
+
 python run.py \
     --dataset_name POKEC_N \
     --grid_search \
     --n_runs 3 \
+    --params_file $hyperparams_dir/hyperparams.yml \
     --experiment_name POKEC_N_grid_search_ours_more_epochs
 
 python run.py \
     --dataset_name POKEC_Z \
     --grid_search \
     --n_runs 3 \
+    --params_file $hyperparams_dir/hyperparams.yml \
     --experiment_name POKEC_Z_grid_search_ours_more_epochs
