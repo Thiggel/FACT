c513a6ce8a2e944f2cf587d2a262635b400a640c
Belitsky Max <scur0209@int4.local.snellius.surf.nl>
2024-06-09 08:09:40 +0200

    Add longer grid search

---
+++ scripts/experiment_2_long.sh
@@ -0,0 +1,11 @@
+python run.py \
+    --dataset_name POKEC_N \
+    --grid_search \
+    --n_runs 3 \
+    --experiment_name POKEC_N_grid_search_ours_more_epochs
+
+python run.py \
+    --dataset_name POKEC_Z \
+    --grid_search \
+    --n_runs 3 \
+    --experiment_name POKEC_Z_grid_search_ours_more_epochs
